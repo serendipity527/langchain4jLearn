6a691debec1c9993b783c43d1be30bee95d82e6f
v-lizhengfan <serendipity_yihu@163.com>
2025-11-21 14:40:40 +0800

    Initial commit: LangChain4j learning project

---
+++ src/main/java/org/example/ragtest/RagTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.ragtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagTestApplication.class, args);
+    }
+
+}
